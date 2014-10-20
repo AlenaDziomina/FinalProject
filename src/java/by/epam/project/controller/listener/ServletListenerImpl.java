bff0be7ba2acb911f0a9c77995d32eca89e6f23e
Alena <groukhelena@gmail.com>
2014-10-20 18:46:57 +0200

    direction classes css menu css countrys columns country command separate dao and query connection level dao country edit and save final dao factory and country edit fill countries fill cities independent selects works directions add hotel add direction

---
+++ src/java/by/epam/project/controller/listener/ServletListenerImpl.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package by.epam.project.controller.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author User
+ */
+public class ServletListenerImpl implements ServletContextListener {
+    
+    private static final Logger LOGGER = Logger.getLogger(ServletListenerImpl.class);
+    @Override
+    public void contextInitialized(ServletContextEvent ev) {
+        ServletContext context = ev.getServletContext();
+        String admin = context.getInitParameter("admin");
+        context.log("Context Initialized with parameter: " + admin);
+    }
+    @Override
+    public void contextDestroyed(ServletContextEvent ev) {
+    }
+}
---
+++ src/java/by/epam/project/controller/listener/SessionListenerImpl.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package by.epam.project.controller.listener;
+
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author User
+ */
+public class SessionListenerImpl implements HttpSessionAttributeListener {
+    
+    private static final Logger LOGGER = Logger.getLogger(SessionListenerImpl.class);
+    
+    /**
+     *
+     * @param ev
+     */
+    @Override
+    public void attributeRemoved(HttpSessionBindingEvent ev) {
+    }
+    
+    /**
+     *
+     * @param ev
+     */
+    @Override
+    public void attributeAdded(HttpSessionBindingEvent ev) {
+        // запись в log-файл или иные действия
+        LOGGER.info("add: " + ev.getClass().getSimpleName() + " : "+ ev.getName()
+        + " : " + ev.getValue());
+    }
+
+    /**
+     *
+     * @param ev
+     */
+    @Override
+    public void attributeReplaced(HttpSessionBindingEvent ev) {
+        // запись в log-файл или иные действия
+        LOGGER.info("replace: " + ev.getClass().getSimpleName() + " : " + ev.getName()
+        + " : " + ev.getValue());
+    }
+}
